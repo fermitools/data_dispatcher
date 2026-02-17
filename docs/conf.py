a5eb4d2bbde80093258d6cae617497860522b078
Alison Peisker <apeisker@fnal.gov>
2026-02-17 14:01:21 -0600

    formatting

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ html_theme_options = {
     "repository_url": "https://github.com/fermitools/data_dispatcher",
     "use_repository_button": True,
     "navigation_depth": 4,  # how many levels of nested headers show in sidebar
-    "collapse_navigation": False
+    "collapse_navigation": False,
     "footer_content_items": []
 }
 
